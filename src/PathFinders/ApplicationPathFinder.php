0b9810218cbf38e2b57ad7870060c68daaf2db9e
Waygou <bruno.falcao@waygou.com>
2019-01-21 21:38:55 +0000

    Apply fixes from StyleCI

--- src/PathFinders/ApplicationPathFinder.php
+++ src/PathFinders/ApplicationPathFinder.php
@@ -4,10 +4,9 @@ namespace Waygou\Flame\PathFinders;
 
 class ApplicationPathFinder
 {
-
-    function __invoke()
+    public function __invoke()
     {
-        return __DIR__ . '/../';
+        return __DIR__.'/../';
 
         return is_dir(base_path('vendor/waygou/flame/src/Features')) ?
                       base_path('vendor/waygou/flame/src/Features') :
